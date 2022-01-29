a7f37cc2c10ed4e96e93074980a763cb59fadacb
Valery Daroshka <Valery_Daroshka@epam.com>
2022-01-29 12:29:31 +0300

    First commit

---
+++ src/main/java/com/github/ValeryAD/examples/App.java
@@ -0,0 +1,13 @@
+package com.github.ValeryAD.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
